ce8b4e174d8f1edee560eb699b0beb1159b92cf2
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-11 18:37:57 +0300

    add solution day02.task1

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int floors = scanner.nextInt();
+        if (floors > 0 && floors <= 4) System.out.println("Малоэтажный дом");
+        else if (floors >= 5 && floors <= 8) System.out.println("Среднеэтажный дом");
+        else if (floors >= 9) System.out.println("Многоэтажный дом");
+        else System.out.println("Ошибка ввода");
     }
 }
