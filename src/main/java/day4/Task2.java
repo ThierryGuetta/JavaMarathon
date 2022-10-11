9c043f3db9674a41473f9a9fd108243440effd05
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-11 20:15:09 +0300

    add solution day04.task2

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -2,6 +2,23 @@ package day4;
 
 public class Task2 {
     public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10000);
+        }
+        int max = array[0];
+        int min = array[0];
+        int multipleOf10 = 0;
+        int sumMultipleOf10 = 0;
 
+        for (int e : array) {
+            if (e > max) max = e;
+            if (e < min) min = e;
+            if (e % 10 == 0) {
+                multipleOf10++;
+                sumMultipleOf10 += e;
+            }
+        }
+        System.out.println(max + "\n" + min + "\n" + multipleOf10 + "\n" + sumMultipleOf10);
     }
 }
