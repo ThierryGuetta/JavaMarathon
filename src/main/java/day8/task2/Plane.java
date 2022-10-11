487c9f681b1508cd4c6e5716b693106b772f01ce
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-12 00:27:38 +0300

    add solution day08.task2

---
+++ src/main/java/day8/task2/Plane.java
@@ -0,0 +1,52 @@
+package day8.task2;
+
+public class Plane {
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Plane(String producer, int year, int length, int weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + producer + ", " + "год выпуска: " + year + ", "
+                + "длина: " + length + ", " + "вес: " + weight + ", "
+                + "количество топлива в баке: " + fuel);
+    }
+
+    public void fillUp(int fuel) {
+        this.fuel += fuel;
+    }
+
+    public static void compareAirplanes(Plane plane1, Plane plane2) {
+        if (plane1.length == plane2.length) System.out.println("Самолеты равны");
+        String s = plane1.length > plane2.length ? "Первый самолет длинней второго" : "Второй самолет длиннее первого";
+        System.out.println(s);
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{" +
+                "producer='" + producer + '\'' +
+                ", year=" + year +
+                ", length=" + length +
+                ", weight=" + weight +
+                ", fuel=" + fuel +
+                '}';
+    }
+}
---
+++ src/main/java/day8/task2/Task2.java
@@ -0,0 +1,8 @@
+package day8.task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Plane plane = new Plane("Boeing", 2019, 25, 100);
+        System.out.println(plane);
+    }
+}
