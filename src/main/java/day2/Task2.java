790f391d4191208754bb16f70f90978781312dff
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-11 18:45:53 +0300

    add solution day02.task2

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        if (first >= second) System.out.println("Некорректный ввод");
+        for (int i = first + 1; i < second; i++) {
+            if (i % 5 == 0 && i % 10 != 0) System.out.println(i);
+        }
     }
 }
 
