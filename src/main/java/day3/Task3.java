80da7afad76bbdb6f37b5b8c3abfb64c1c572062
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-11 19:26:15 +0300

    add solution day03.task3

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            double x = scanner.nextInt();
+            double y = scanner.nextInt();
+            if (y == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(x / y);
+        }
     }
 }
