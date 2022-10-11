b1e50ea77a0a57bcf1fb4131709b47e372fc139e
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-12 00:23:21 +0300

    add solution day08.task1

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,20 @@
 package day8;
 
+
 public class Task1 {
     public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println("1 StringBuilder Time: " + (System.currentTimeMillis() - time));
 
+        long time2 = System.currentTimeMillis();
+        String result = "";
+        for (int i = 0; i <= 20000; i++) {
+            result += i + " ";
+        }
+        System.out.println("2 String Time: " + (System.currentTimeMillis() - time2));
     }
 }
