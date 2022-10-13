aa52c2a175af7203e044585a1dd2fe968369c4f8
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-13 20:06:23 +0300

    add solution day12.task1

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,22 @@
 package day12;
 
+import java.util.*;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("BMW");
+        list.add("LADA");
+        list.add("Corvette");
+        list.add("Toyota");
+        list.add("Opel");
 
+        System.out.println(list);
+
+        list.add(2, "Ford");
+
+        list.remove(0);
+
+        System.out.println(list);
     }
 }
