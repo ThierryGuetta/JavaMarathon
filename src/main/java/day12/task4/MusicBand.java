a0672cc860fffe180d5cd39c010674e25d8c02ca
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-13 21:40:17 +0300

    add solution day12.task4

---
+++ src/main/java/day12/task4/MusicBand.java
@@ -0,0 +1,36 @@
+package day12.task4;
+
+import java.util.ArrayList;
+
+public class MusicBand {
+    private String name;
+    private int year;
+    private ArrayList<String> bandMembers = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getBandMembers() {
+        return bandMembers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public MusicBand(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public void addMember(String member){
+        bandMembers.add(member);
+    }
+    public void printMembers(){
+        System.out.println(name + " " + getBandMembers());
+    }
+    public static void transferMembers(MusicBand musicBand, MusicBand musicBand2){
+        musicBand.getBandMembers().addAll(musicBand2.getBandMembers());
+    }
+}
--- src/main/java/day12/task4/Task4.java
+++ src/main/java/day12/task4/Task4.java
@@ -2,6 +2,25 @@ package day12.task4;
 
 public class Task4 {
     public static void main(String[] args) {
+        MusicBand LP = new MusicBand("Linkin park", 1999);
+        MusicBand SOAD = new MusicBand("System of a Down", 1998);
 
+        LP.addMember("Bob");
+        LP.addMember("Rob");
+        LP.addMember("Max");
+        LP.addMember("Alex");
+        LP.addMember("Iven");
+        SOAD.addMember("Igor");
+        SOAD.addMember("Pasha");
+        SOAD.addMember("Oleg");
+        SOAD.addMember("Gesha");
+        SOAD.addMember("Vova");
+
+        LP.printMembers();
+        SOAD.printMembers();
+
+        MusicBand.transferMembers(LP, SOAD);
+
+        LP.printMembers();
     }
 }
