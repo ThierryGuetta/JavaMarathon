2ac45fbef22ec4ba6cbe5695e01339204b9c91b7
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-14 20:38:18 +0300

    add solution day16.task1

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,26 @@
 package day16;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws IOException{
+        File file = new File("E:\\lessons\\JavaMarathon\\src\\main\\java\\day16\\text.txt");
+        printResult(file);
+    }
+
+    public static void printResult(File file) throws IOException {
+        Scanner scanner = new Scanner(file);
+        int sum = 0;
+        int length = 0;
+        while (scanner.hasNextLine()) {
+            String[] array = scanner.nextLine().split(" ");
+            for (String num : array) {
+                sum += Integer.parseInt(num);
+                length++;
+            }
+        }
+        System.out.println(sum / length);
     }
 }
