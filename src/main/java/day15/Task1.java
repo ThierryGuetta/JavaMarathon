bcc2b7df4203f5be2b51fa18ffc46bc7a43e1d0c
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-14 19:32:37 +0300

    add solution day15.task1

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,23 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+        File input = new File("E:\\lessons\\JavaMarathon\\src\\main\\resources\\shoes.csv");
+        Scanner scanner = new Scanner(input);
+
+        File output = new File("C:\\Users\\Андрей\\Desktop\\text.txt");
+        PrintWriter pw = new PrintWriter(output);
+
+        while (scanner.hasNextLine()) {
+            String[] array = scanner.nextLine().split(";");
+            if (Integer.parseInt(array[2]) == 0) {
+                pw.println(array[0] + " " + array[1] + " " + array[2]);
+            }
+        }
+        pw.close();
+        scanner.close();
     }
 }
