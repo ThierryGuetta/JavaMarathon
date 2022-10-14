e6523e28c61b52c2b5767686b3e215329387d01f
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-14 15:51:19 +0300

    add solution day14.task1

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,32 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("E:\\lessons\\JavaMarathon\\src\\main\\java\\day14\\text.txt");
+        printSumDigits(file);
 
+    }
+
+    public static void printSumDigits(File file) {
+        int sum = 0;
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] nums = line.split(" ");
+            if (nums.length != 10) throw new RuntimeException("Неккоректный входной файл");
+            for (String num : nums) {
+                sum += Integer.parseInt(num);
+            }
+            scanner.close();
+            System.out.println(sum);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
