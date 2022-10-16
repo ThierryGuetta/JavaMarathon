4dc7adc81f74fbc526bde60321ab3967b649b05c
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-16 17:31:10 +0300

    add solution day18.task1

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,19 +1,15 @@
 package day18;
 
-/**
- * @author Neil Alishev
- */
 public class Task1 {
     public static void main(String[] args) {
         int[] numbers = {-402, -50, -249, -15, -665, -311};
-
-        System.out.println(recursionSum(numbers, 0));
+        int i = 0;
+        System.out.println(recursionSum(numbers, i));
     }
 
-    public static int recursionSum(int[] numbers, int i) {
-        if (i == numbers.length)
-            return 0;
+    public static int recursionSum(int[] number, int i) {
+        if ( i == number.length) return 0;
 
-        return numbers[i] + recursionSum(numbers, i + 1);
+        return number[i] += recursionSum(number, i + 1);
     }
 }
